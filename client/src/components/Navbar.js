aa7cf42142443200c86ab4a1badc2a6244b68108
tehpunisher456 <55720542+tehpunisher456@users.noreply.github.com>
2020-04-25 11:01:00 -0700

    added working links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link } from "react-router-dom"
 
 const Navbar = () => {
   return(
@@ -12,24 +13,13 @@ const Navbar = () => {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" ><Link to = "/profile">Profile </Link></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a className="nav-link" href="#"><Link to = "/Login">Login</Link></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a className="nav-link" href="#"><Link to = "/Signup">Signup</Link></a>
       </li>
     </ul>
     <form className="form-inline my-2 my-lg-0">
