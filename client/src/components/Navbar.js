db3f5b4021743e9f7a9997cbad960370f4184760
jihyeonoh12 <jihyeonoh12@gmail.com>
2020-04-27 20:40:19 -0700

    navbar fixed and added home elements

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,33 +4,25 @@ import {Link } from "react-router-dom"
 const Navbar = () => {
   return(
   <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link to="/" className="navbar-brand">Navbar </Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <Link to= "/profile"> className="nav-link" > Profile </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Login" className="nav-link">Login</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Signup" className="nav-link"> Signup</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/create" className="nav-link">Create Post</Link>
-      </li>
+{/* <ul id="dropdown1" class="dropdown-content">
+  <li><a href="/profile">Profile</a></li>
+  <li><a href="#!">two</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">three</a></li>
+</ul> */}
+<nav>
+  <div className="nav-wrapper">
+    <Link to="#!" class="brand-logo">Logo</Link>>
+    <ul id="nav-mobile" className="right">
+    <li><Link to="/Profile">Profile</Link>></li>
+    <li><Link to="/Login">Login</Link>></li>
+    <li><Link to="/Signup">Signup</Link>></li> 
+    <li><Link to="/create">Create Post</Link>></li> 
     </ul>
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
+
 </div>
 )
 }
